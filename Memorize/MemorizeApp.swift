e344e2d5af6556a2351a97e9d1c036ad4ab88f56
Samuel Brehm <samuelbrehm@gmail.com>
2021-03-29 09:39:47 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Samuel Brehm on 29/03/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
